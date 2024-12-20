70a279cbf47388604a39bdc414bd2d37e843970c
Thibaut <thibautstachnick@gmail.com>
2024-12-20 21:30:08 +0100

    [config]: add tailwind css to the app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
 })
