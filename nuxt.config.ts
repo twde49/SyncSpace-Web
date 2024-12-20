391d7ecd7d299becf9ebfd95a34fde0a5e0e22d0
Thibaut <thibautstachnick@gmail.com>
2024-12-20 21:54:48 +0100

    [config]: add flowbite to the app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,18 @@
 export default defineNuxtConfig({
+  app:{
+    head: {
+        script: [
+          { src: 'https://cdn.jsdelivr.net/npm/flowbite/dist/flowbite.min.js', defer: true }
+        ]
+      }
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
-      plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-      },
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
     },
+  },
 })
