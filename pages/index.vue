ea28312d22ead5ed1180180c2b04370c53c0c1b6
Thibaut <thibautstachnick@gmail.com>
2024-12-22 11:27:47 +0100

    [home]:add Hero component to the home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
-    <Navbar />
+  <Navbar />
+  <Hero />
 </template>
 
 <script setup lang="ts">
-import Navbar from '~/components/notLogged/Navbar.vue'
+import Navbar from '~/components/notLogged/Navbar.vue';
+import Hero from '~/components/notLogged/Hero.vue';
 </script>
